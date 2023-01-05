631b4b5fb7f124e6f607da98ac1ff305f33e8545
lumarin2802 <marinperezluciana@gmail.com>
2023-01-05 18:52:23 +0000

    las cartas se ordenan en la consola

--- src/app.js
+++ src/app.js
@@ -7,83 +7,100 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var palos = ["♦", "♥", "♠", "♣"];
 
-  //esta funcion genera UNA carta ramdom
-  function ramdomCard() {
-    const palos = ["♦", "♥", "♠", "♣"];
-    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const numeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "As"
+  ];
 
-    let indexPalo = Math.floor(Math.random() * palos.length);
-    let indexNumero = Math.floor(Math.random() * numeros.length);
+  let todasCartas = [];
 
-    console.log(palos[indexPalo], numeros[indexNumero]);
-    //obteniendo elementos
-    // let top = document.getElementById("top");
-    // let number = document.getElementById("number");
-    // let bottom = document.getElementById("bottom");
+  for (let i = 1; i <= 5; i++) {
+    let indexPalos = Math.floor(Math.random() * palos.length);
+    let indexNumeros = Math.floor(Math.random() * numeros.length);
+    //aca obtengo el palo
+    console.log(palos[indexPalos]);
+    //aca obtengo el numero
+    console.log(numeros[indexNumeros]);
+    //generamos la variable carta que es el array que tendra el conjunto de datos; palos y numeros
+    let carta = [numeros[indexNumeros], palos[indexPalos]];
+    console.log(carta);
+    //llamamos al array y con push ingresamos cada conjunto de datos que se genera con iteracion
+    todasCartas.push(carta);
+    // console.log(todasCartas);
 
-    //ingresar valores
-    // top.innerHTML = palos[indexPalo];
-    // number.innerHTML = numeros[indexNumero];
-    // bottom.innerHTML = palos[indexPalo];
-
-    //declaramos un array que tendra todas las cartas
-    let todasCartas = [];
-
-    for (let i = 1; i <= 5; i++) {
-      let indexPalo = Math.floor(Math.random() * palos.length);
-      let indexNumero = Math.floor(Math.random() * numeros.length);
-      //aca obtengo el numero
-      console.log(palos[indexPalo]);
-      //aca obtengo el numero
-      console.log(numeros[indexNumero]);
-      //generamos la variable carta que es el array que tendra el conjuntode datos; palos y numeros
-      let carta = [palos[indexPalo], numeros[indexNumero]];
-      console.log(carta);
-      //llamamos el array y con push ingresamos cada conjunto de datos que se genera con iteracion.
-
-      todasCartas.push(carta);
-      console.log(todasCartas);
-
-      if (palos[indexPalo] === "♥" || palos[indexPalo] === "♦") {
-        document.body.innerHTML += `<div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item text-danger" id="top">${palos[indexPalo]}</li>
-          <li class="list-group-item text-danger" id="number">${numeros[indexNumero]}</li>
-          <li class="list-group-item text-danger" id="bottom">${palos[indexPalo]}</li>
-        </ul>
-      </div>`;
-      } else {
-        document.body.innerHTML += `<div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" id="top">${palos[indexPalo]}</li>
-          <li class="list-group-item" id="number">${numeros[indexNumero]}</li>
-          <li class="list-group-item" id="bottom">${palos[indexPalo]}</li>
-        </ul>
-      </div>`;
-      }
+    if (palos[indexPalos] === "♥" || palos[indexPalos] === "♦") {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item text-danger" id="top">${palos[indexPalos]}</li>
+      <li class="list-group-item text-danger" id="number">${numeros[indexNumeros]}</li>
+      <li class="list-group-item text-danger" id="bottom">${palos[indexPalos]}</li>
+    </ul>
+  </div>`;
+    } else {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item" id="top">${palos[indexPalos]}</li>
+        <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
+        <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
+      </ul>
+    </div>`;
     }
-    //
   }
 
-  ramdomCard();
-};
-const bubbleSort = arr => {
-  let wall = arr.length - 1; //iniciamos el wall o muro al final del array
+  let wall = todasCartas.length - 1;
+
   while (wall > 0) {
     let index = 0;
     while (index < wall) {
-      //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
-      if (arr[index] > arr[index + 1]) {
-        let aux = arr[index];
-        arr[index] = arr[index + 1];
-        arr[index + 1] = aux;
+      let temp = todasCartas[index].slice(0, 1);
+      let temp1 = todasCartas[index + 1].slice(0, 1);
+
+      //let temp1 = arrCartas[index + 1][1];
+
+      if (temp == "A") {
+        temp = 1;
+      } else if (temp == "J") {
+        temp = 10;
+      } else if (temp == "Q") {
+        temp = 11;
+      } else if (temp == "K") {
+        temp = 12;
+      }
+
+      if (temp1 == "A") {
+        temp1 = 1;
+      } else if (temp1 == "J") {
+        temp1 = 10;
+      } else if (temp1 == "Q") {
+        temp1 = 11;
+      } else if (temp1 == "K") {
+        temp1 = 12;
+      }
+
+      if (temp > temp1) {
+        let aux = todasCartas[index];
+        todasCartas[index] = todasCartas[index + 1];
+        todasCartas[index + 1] = aux;
       }
       index++;
     }
-    wall--; //disminuir la pared para optimizar
+    wall--;
+    console.log(todasCartas);
   }
-  console.log(bubbleSort());
-  return arr;
+
+  // dibujarCartas(arrCartas);
+  // return arrCartas;
 };
