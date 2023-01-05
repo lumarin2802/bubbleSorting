97eff34510f38b20b2f891088ae35ea4b9a59056
lumarin2802 <marinperezluciana@gmail.com>
2023-01-05 19:33:19 +0000

    funcionan los botones y se sortean las cartas

--- src/app.js
+++ src/app.js
@@ -7,100 +7,145 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  var palos = ["♦", "♥", "♠", "♣"];
 
-  const numeros = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "K",
-    "Q",
-    "J",
-    "As"
-  ];
+  function randomCard() {
+    var palos = ["♦", "♥", "♠", "♣"];
 
-  let todasCartas = [];
+    const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
 
-  for (let i = 1; i <= 5; i++) {
-    let indexPalos = Math.floor(Math.random() * palos.length);
-    let indexNumeros = Math.floor(Math.random() * numeros.length);
-    //aca obtengo el palo
-    console.log(palos[indexPalos]);
-    //aca obtengo el numero
-    console.log(numeros[indexNumeros]);
-    //generamos la variable carta que es el array que tendra el conjunto de datos; palos y numeros
-    let carta = [numeros[indexNumeros], palos[indexPalos]];
-    console.log(carta);
-    //llamamos al array y con push ingresamos cada conjunto de datos que se genera con iteracion
-    todasCartas.push(carta);
-    // console.log(todasCartas);
+    //Obtengo Numero
+    let palosRandom = palos[Math.floor(Math.random() * palos.length)];
+    let numerosRandom = numeros[Math.floor(Math.random() * numeros.length)];
 
-    if (palos[indexPalos] === "♥" || palos[indexPalos] === "♦") {
-      document.body.innerHTML += `<div class="card" style="width: 18rem;">
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item text-danger" id="top">${palos[indexPalos]}</li>
-      <li class="list-group-item text-danger" id="number">${numeros[indexNumeros]}</li>
-      <li class="list-group-item text-danger" id="bottom">${palos[indexPalos]}</li>
-    </ul>
-  </div>`;
-    } else {
-      document.body.innerHTML += `<div class="card" style="width: 18rem;">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" id="top">${palos[indexPalos]}</li>
-        <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
-        <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
-      </ul>
-    </div>`;
-    }
+    let color = palosRandom == "♥" || numerosRandom == "♦" ? "text-danger" : "";
+
+    document.getElementById(
+      "cartasAleatorias"
+    ).innerHTML += `<div class="col-auto rounded">
+    <!-- Card Body -->
+    <div
+      class="px-3 my-3 rounded m-auto bg-white "
+      style="width: 6rem; height: 10rem"
+      id="card"
+    >
+      <!-- Upper icon-->
+      <div class="row  ${color}" style="font-size: 22px;">
+        <span id="upper">${palosRandom}</span>
+      </div>
+      <!-- Number -->
+      <div class="row text-center my-3 ${color}" style="font-size: 45px;">
+        <span id="number">${numerosRandom}</span>
+      </div>
+      <!-- Lower icon-->
+      <div
+        class="d-flex flex-row  justify-content-end float-right ${color}"
+        style="font-size: 22px; "
+      >
+        <span style="transform: rotate(180deg);" id="bottom">${palosRandom}</span>
+      </div>
+    </div>
+</div>`;
+
+    let aleatorio = [numerosRandom, palosRandom];
+
+    return aleatorio;
   }
 
-  let wall = todasCartas.length - 1;
+  let arrayCartas = [];
 
-  while (wall > 0) {
-    let index = 0;
-    while (index < wall) {
-      let temp = todasCartas[index].slice(0, 1);
-      let temp1 = todasCartas[index + 1].slice(0, 1);
-
-      //let temp1 = arrCartas[index + 1][1];
-
-      if (temp == "A") {
-        temp = 1;
-      } else if (temp == "J") {
-        temp = 10;
-      } else if (temp == "Q") {
-        temp = 11;
-      } else if (temp == "K") {
-        temp = 12;
-      }
-
-      if (temp1 == "A") {
-        temp1 = 1;
-      } else if (temp1 == "J") {
-        temp1 = 10;
-      } else if (temp1 == "Q") {
-        temp1 = 11;
-      } else if (temp1 == "K") {
-        temp1 = 12;
-      }
-
-      if (temp > temp1) {
-        let aux = todasCartas[index];
-        todasCartas[index] = todasCartas[index + 1];
-        todasCartas[index + 1] = aux;
-      }
-      index++;
+  //Generar Array de Cartas
+  function generarLista() {
+    document.getElementById("cartasAleatorias").innerHTML = "";
+    arrayCartas = [];
+    let carntidadDeCartas = document.getElementById("cantidad-cartas").value;
+    for (let i = 0; i < carntidadDeCartas; i++) {
+      let cartaAleatoria = randomCard();
+      arrayCartas.push(cartaAleatoria);
+      console.log(arrayCartas);
     }
-    wall--;
-    console.log(todasCartas);
+    return arrayCartas;
   }
 
-  // dibujarCartas(arrCartas);
-  // return arrCartas;
+  //Llamamos a la funcion mediante el boton
+  document.getElementById("draw").addEventListener("click", generarLista);
+  document.getElementById("sort").addEventListener("click", function() {
+    let wall = arrayCartas.length - 1;
+
+    while (wall > 0) {
+      let index = 0;
+      while (index < wall) {
+        //let temp1 = arrCartas[index + 1][1];
+
+        if (arrayCartas[index + 1][0] == "K") {
+          arrayCartas[index + 1][0] = 13;
+        } else if (arrayCartas[index + 1][0] == "Q") {
+          arrayCartas[index + 1][0] = 12;
+        } else if (arrayCartas[index + 1][0] == "J") {
+          arrayCartas[index + 1][0] = 11;
+        } else if (arrayCartas[index + 1][0] == "A") {
+          arrayCartas[index + 1][0] = 1;
+        }
+
+        if (arrayCartas[index][0] == "K") {
+          arrayCartas[index][0] = 13;
+        } else if (arrayCartas[index][0] == "Q") {
+          arrayCartas[index][0] = 12;
+        } else if (arrayCartas[index][0] == "J") {
+          arrayCartas[index][0] = 11;
+        } else if (arrayCartas[index][0] == "A") {
+          arrayCartas[index][0] = 1;
+        }
+
+        if (arrayCartas[index][0] > arrayCartas[index + 1][0]) {
+          let aux = arrayCartas[index];
+          arrayCartas[index] = arrayCartas[index + 1];
+          arrayCartas[index + 1] = aux;
+        }
+        index++;
+      }
+      wall--;
+    }
+    document.getElementById("cartasOrdenadas").innerHTML = "";
+    for (let index = 0; index < arrayCartas.length; index++) {
+      if (arrayCartas[index][0] == 13) {
+        arrayCartas[index][0] = "K";
+      } else if (arrayCartas[index][0] == 12) {
+        arrayCartas[index][0] = "Q";
+      } else if (arrayCartas[index][0] == 11) {
+        arrayCartas[index][0] = "J";
+      } else if (arrayCartas[index][0] == 1) {
+        arrayCartas[index][0] = "A";
+      }
+      let color =
+        arrayCartas[index][1] == "♥" || arrayCartas[index][0] == "♦"
+          ? "text-danger"
+          : "";
+      document.getElementById(
+        "cartasOrdenadas"
+      ).innerHTML += `<div class="col-auto rounded">
+                <!-- Card Body -->
+                <div
+                  class="px-3 my-3 rounded m-auto bg-white "
+                  style="width: 6rem; height: 10rem"
+                  id="card"
+                >
+                  <!-- Upper icon-->
+                  <div class="row  ${color}" style="font-size: 22px;">
+                    <span id="upper">${arrayCartas[index][1]}</span>
+                  </div>
+                  <!-- Number -->
+                  <div class="row text-center my-3 ${color}" style="font-size: 45px;">
+                    <span id="number">${arrayCartas[index][0]}</span>
+                  </div>
+                  <!-- Lower icon-->
+                  <div
+                    class="d-flex flex-row  justify-content-end float-right ${color}"
+                    style="font-size: 22px; "
+                  >
+                    <span style="transform: rotate(180deg);" id="bottom">${arrayCartas[index][1]}</span>
+                  </div>
+                </div>
+            </div>`;
+    }
+  });
 };
